2a3ad8b0eb9dc5cc8c27b617f57eec549c989007
RUNNERUP <raj2011@163.com>
2018-09-07 00:41:00 +0800

    1. 引用circle-menu 新增帮助功能

--- VirtualMechanicsLab/src/views/VmlWorkbench.vue
+++ VirtualMechanicsLab/src/views/VmlWorkbench.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="vml-workbench" style="height:100%:width:100%">
-    <div id='menu'>
+    <!-- <div id='menu'>
       <el-menu mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
         <el-submenu index="scene">
           <template slot="title">场景</template>
@@ -28,12 +28,22 @@
           <a href="#" target="_blank">关于</a>
         </el-menu-item>
       </el-menu>
-    </div>
-    <div id='workbench-toolbar'>
+    </div> -->
+    <header id='vml-header'>
+
+    </header>
+    <circle-menu class='circle-menu-style' type="bottom" :number="4" animate="animated jello" circle>
+      <a slot="item_btn" class="question el-icon-question" title="帮助"></a>
+      <a slot="item_1" class='question-font' @click="handleAbout">关于</a>
+      <a slot="item_2" class='question-font'>问答</a>
+      <a slot="item_3" class='question-font'>建议</a>
+      <a slot="item_4" class='question-font'>支持</a>
+    </circle-menu>
+    <!-- <div id='workbench-toolbar'>
       <el-button type="primary" size="mini" circle title="编辑" @click='sceneEdit' icon="el-icon-setting"></el-button>
       <el-button type="primary" size="mini" circle title='编辑&运行' @click='sceneHalf' icon="el-icon-menu"></el-button>
       <el-button type="primary" size="mini" circle title='运行' @click='sceneRunning' icon="el-icon-d-arrow-right"></el-button>
-    </div>
+    </div> -->
     <div id='main-workbench'>
       <el-row style="height:100%">
         <el-col style="height:100%" :span="sceneEditorSpan">
@@ -52,6 +62,7 @@ import * as types from '@/modules-constant.js'
 import { mapGetters, mapMutations, mapActions } from 'vuex'
 import SceneEditor from './scene-editor/SceneEditor.vue'
 import SceneRunning from './SceneRunning.vue'
+import CircleMenu from 'vue-circle-menu'
 export default {
   name: 'vml-workbench',
   data: function () {
@@ -127,6 +138,15 @@ export default {
     sceneRunning: function () {
       this.sceneEditorSpan = 0;
       this.sceneRunningSpan = 24
+    },
+    handleAbout: function () {
+      this.$confirm('这是一个模拟物理场景的实验室！', '关于', {
+        center: true,
+        showConfirmButton: false,
+        showCancelButton: false,
+        showClose: true,
+        type: 'info '
+      })
     }
   },
   beforeMount: function () {
@@ -135,17 +155,62 @@ export default {
   },
   components: {
     SceneEditor,
-    SceneRunning
+    SceneRunning,
+    CircleMenu
   }
 }
 </script>
 <style>
+#vml-header {
+  background-color: #3399cc;
+  height: 30px;
+}
+.circle-menu-style {
+  position: absolute;
+  top: 3px;
+  right: 10px;
+}
+.question {
+  font-size: 28px;
+  margin-left: -8px;
+  margin-top: -3px;
+}
+.circle-menu-style /deep/ .oy-menu-btn {
+  line-height: 0px;
+  width: 24px;
+  height: 24px;
+  background: #2b59c5;
+}
+
+.circle-menu-style /deep/ .bottom .oy-menu-group .btn-list .oy-menu-item {
+  left: auto;
+  line-height: inherit;
+  width: 40px;
+  height: 40px;
+}
+.circle-menu-style /deep/ .bottom .oy-menu-group.open .btn-list .oy-menu-item.oy-menu-item_1 {
+  top: 35px;
+}
+.circle-menu-style /deep/ .bottom .oy-menu-group.open .btn-list .oy-menu-item.oy-menu-item_2 {
+  top: 90px;
+}
+.circle-menu-style /deep/ .bottom .oy-menu-group.open .btn-list .oy-menu-item.oy-menu-item_3 {
+  top: 145px;
+}
+.circle-menu-style /deep/ .bottom .oy-menu-group.open .btn-list .oy-menu-item.oy-menu-item_4 {
+  top: 200px;
+}
+.question-font {
+  line-height: 40px;
+  color: #020e1a;
+}
 #main-workbench {
   position: absolute;
   top: 60px;
   bottom: 0px;
   width: 100%;
 }
+
 #workbench-toolbar {
   position: absolute;
   top: 10px;
