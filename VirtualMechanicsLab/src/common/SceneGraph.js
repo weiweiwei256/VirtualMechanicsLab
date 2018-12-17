f448266a524f690ab15f5aafd06353c651c01f25
RUNNERUP <raj2011@163.com>
2018-12-18 02:53:17 +0800

    1. 圆物体拖动约束

--- VirtualMechanicsLab/src/common/SceneGraph.js
+++ VirtualMechanicsLab/src/common/SceneGraph.js
@@ -1,4 +1,5 @@
 import utility from '@/common/utility.js'
+import * as types from '@/modules-constant.js'
 // 重写原有方法
 mxCell.prototype.getAttribute = function(name, defaultValue) {
   let cellValue = this.getValue()
@@ -24,4 +25,24 @@ mxGraphHandler.prototype.guidesEnabled = true
 // Alt 使拖动辅助失效
 mxGuide.prototype.isEnabledForEvent = function(evt) {
   return !mxEvent.isAltDown(evt)
+}
+// 对于圆形的拖拽进行约束
+mxVertexHandler.prototype.union = function(bounds, dx, dy, index, gridEnabled, scale, tr, constrained, centered) {
+  if (this.state.cell.value.general.type == types.CIRCLE) {
+    let minOffset = Math.min(dx, dy)
+    dx = minOffset
+    dy = minOffset
+  }
+  var x = bounds.x + bounds.width + dx
+  var y = bounds.y + bounds.height + dy
+
+  if (gridEnabled) {
+    x = this.graph.snap(x / scale) * scale
+    y = this.graph.snap(y / scale) * scale
+  }
+
+  var rect = new mxRectangle(bounds.x, bounds.y, 0, 0)
+  rect.add(new mxRectangle(x, y, 0, 0))
+
+  return rect
 }
